eabe94b126fbc1bfd3c394ed4b462ea463034b31
erianiel <valentina.gravil@gmail.com>
2025-07-30 17:51:58 +0200

    feat: install react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./styles.css";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
@@ -18,6 +19,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
