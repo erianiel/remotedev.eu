cc18e9549daa808650cfbe64af099d3d6593d1ed
erianiel <valentina.gravil@gmail.com>
2025-08-13 10:35:47 +0200

    refactor: add env variable and refactor url

--- src/services/apiJobs.ts
+++ src/services/apiJobs.ts
@@ -1,5 +1,7 @@
 import type { PaginationState, SortingState } from "@tanstack/react-table";
 
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+
 export async function getJobs(options: {
   pagination: PaginationState;
   sorting: SortingState;
@@ -11,16 +13,18 @@ export async function getJobs(options: {
     ? `${options.sorting[0].id}.${options.sorting[0].desc ? "desc" : "asc"}`
     : "created_at.desc"; // Default sort
 
-  const response = await fetch(
-    `https://lsonohvayyootvjagxvw.supabase.co/functions/v1/jobs?page=${page}&pageSize=${pageSize}&sort=${sort}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      signal: options.signal,
-    }
-  );
+  const baseUrl = new URL(`${SUPABASE_URL}/functions/v1/jobs`);
+  baseUrl.searchParams.set("page", String(page));
+  baseUrl.searchParams.set("pageSize", String(pageSize));
+  baseUrl.searchParams.set("sort", sort);
+
+  const response = await fetch(baseUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    signal: options.signal,
+  });
 
   if (!response.ok) {
     throw new Error("Failed to fetch jobs");
